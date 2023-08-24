8fb84208cd27c99029248d61695208ca4bfa8459
Anton Maslikov <totosa9494@gmail.com>
2023-08-24 21:52:29 +0300

    удалил консоли подправил 3.0+ желтые ошибки

--- src/components/TasksFilter.tsx
+++ src/components/TasksFilter.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import { TBtnIsSelectedProps, TFooterProps } from "./types/types";
+import { TFooterProps } from "./types/types";
 export default function TaskFilters({
   activeFilter,
   onClickSetActiveFilter
 }: TFooterProps) {
-  const [btnIsSelected, setBtnIsSelected] = useState<TBtnIsSelectedProps>("");
   const arrayOfNamesForBtns = ["All", "Active", "Completed"];
   return (
     <ul className="filters">
